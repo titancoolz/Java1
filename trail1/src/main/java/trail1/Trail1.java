1a103a5c22b3f839134b8018ec7d83b2939d67ef
titancoolz <basheeruddin112@gmail.com>
2020-08-04 20:18:05 +0530

    comit2

--- trail1/src/main/java/trail1/Trail1.java
+++ trail1/src/main/java/trail1/Trail1.java
@@ -1,11 +1,18 @@
 package trail1;
 
-public class Trail1 {
+public class Trail1 extends class2{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("helloworld");
+		//System.out.print("helloworld");
+		class2 a=new class2();
+		pr();
 
+	int p=1231_2;
+	
+		
 	}
 
 }
+
+
---
+++ trail1/src/main/java/trail1/class2.java
@@ -0,0 +1,8 @@
+package trail1;
+
+public class class2 {
+public static void pr() {System.out.print("helloworldclass2");
+
+
+}
+}
