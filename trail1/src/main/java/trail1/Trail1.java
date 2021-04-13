0fcda73520af067f51e30b62860f99d88c689a67
titancoolz <basheeruddin112@gmail.com>
2021-04-13 23:36:20 +0530

    testing

--- trail1/src/main/java/trail1/Trail1.java
+++ trail1/src/main/java/trail1/Trail1.java
@@ -5,10 +5,14 @@ public class Trail1 extends class2{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//System.out.print("helloworld");
-		class2 a=new class2();
-		pr();
-
+		//class2 a=new class2();
 		
+		//pr();
+		char a=33;
+		char b= 1;
+		char c=1;
+
+		System.out.print(a+b);
 	}
 
 }
